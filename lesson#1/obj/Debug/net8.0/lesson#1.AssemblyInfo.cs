7878ddc4a8bfa5feb6ab6c1ed82867105a5a1b4b
ArtsiomArchiptsov <158454533+ArtsiomArchiptsov@users.noreply.github.com>
2024-02-10 11:51:34 +0300

    Пример №1. Объявление переменной и вывод ее на экран(написан код)

--- lesson#1/obj/Debug/net8.0/lesson#1.AssemblyInfo.cs
+++ lesson#1/obj/Debug/net8.0/lesson#1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lesson#1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14ba588964dca0cffab6a4c14ea8a0f2f85427d")]
 [assembly: System.Reflection.AssemblyProductAttribute("lesson#1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lesson#1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
