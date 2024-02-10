c9adae533add1b3429a56d313fc18569bd154127
ArtsiomArchiptsov <158454533+ArtsiomArchiptsov@users.noreply.github.com>
2024-02-10 19:12:47 +0300

    Задача на подбор максимального веса

---
+++ lesson#4/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Cоставить программу для выбора максимального веса.
+*/
+
+int num1 = 61;
+int num2 = 45;
+int num3 = 54;
+int num4 = 64;
+int num5 = 78;
+
+int max = num1;
+if (max < num2)
+{
+    max = num2;
+}
+if (max < num3)
+{
+    max = num3;
+
+}
+if (max < num4)
+{
+    max = num4;
+}
+if (max < num5)
+{
+    max = num5;
+}
+Console.WriteLine(max);
