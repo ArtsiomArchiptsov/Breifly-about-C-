085985fc712b73aae8c249bc33c8151780c40de7
ArtsiomArchiptsov <158454533+ArtsiomArchiptsov@users.noreply.github.com>
2024-02-10 18:54:03 +0300

    Написан код к условию задачи - lesson#3

--- lesson#3/Program.cs
+++ lesson#3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача №3. 
+Даны два целых числа. Вывести на экран большее из них.
+*/
+
+int num1 = 4;
+int num2 = 8;
+if (num1 > num2){
+    Console.WriteLine(num1);
+}
+
+else{
+    Console.WriteLine(num2);
+}
+
