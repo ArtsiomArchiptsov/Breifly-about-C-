6a4a5c63779083040bf845d7044dc33b55856efb
ArtsiomArchiptsov <158454533+ArtsiomArchiptsov@users.noreply.github.com>
2024-02-10 19:29:49 +0300

    Представлен код с выводом чисел в столбец и в строку - lesson#5

--- lesson#5/Program.cs
+++ lesson#5/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Дано натуральное число. Вывести все значения от 1 до N.
+*/
+
+int numbers = 45;
+int count = 1;
+while (count <= numbers)
+{
+    Console.WriteLine(count);
+    //Console.Write(+count + " ");
+    count = count + 1;
+}
+
